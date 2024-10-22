6e54b43af8f8cb6ac6ce97b0967d2501b0e5aec9
sandeep-kr-kgp <sandeepkr.kgp@gmail.com>
2024-10-22 15:42:15 +0530

    commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import './style.css';
+
+function init() {
+  const params = new URLSearchParams(window.location.search);
+  const text = params.get('text') ?? 'Hello World';
+  document.querySelector('#app').innerHTML = `
+    ${text
+      .split('-')
+      .map((word) => {
+        return `<div class="text">
+        ${word
+          .split('')
+          .map((c) => `<div class="character">${c}</div>`)
+          .join('')}
+        </div>`;
+      })
+      .join('')}
+`;
+  document.querySelectorAll('.character').forEach((c) => {
+    c.addEventListener('mouseover', function (e) {
+      if (e.target.classList.contains('glowing'))
+        e.target.classList.remove('glowing');
+      else e.target.classList.add('glowing');
+    });
+  });
+}
+init();
